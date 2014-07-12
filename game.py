fe8db256f8805e3cf440f80ecfcecedeb3967892
leetea <lktea@uci.edu>
2014-07-11 18:24:47 -0700

    First Commit

---
+++ game.py
@@ -0,0 +1,5 @@
+from sys import exit
+from sys import math
+
+def entrance()
+    Print "\tYou arrive at the doorstep of a eerie-looking house several blocks from your house. You notice that the lights and TV are on, but there is no car in the driveway; you assume that no one is home.\t\nYou are also an experienced burgular who steals mechandise to go feed your gambling addiction at the local casino. Todays is one of those days; this place seemed easy enough on the outside!" 
